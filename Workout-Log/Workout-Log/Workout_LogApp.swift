320292c1c903044bfad779df52402c989ab1166f
jonathanko500 <jkko9010@gmail.com>
2023-11-30 15:59:04 -0800

    created new app

---
+++ Workout-Log/Workout-Log/Workout_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_LogApp.swift
+//  Workout-Log
+//
+//  Created by Jonathan Ko on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Workout_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

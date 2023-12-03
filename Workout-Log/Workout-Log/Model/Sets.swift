82ff4dce3c0a9ba5fa0484773261e2561fa51ebd
jonathanko500 <jkko9010@gmail.com>
2023-12-03 01:00:53 -0800

    setmaker takes user input

--- Workout-Log/Workout-Log/Model/Sets.swift
+++ Workout-Log/Workout-Log/Model/Sets.swift
@@ -38,3 +38,31 @@ extension Set {
 }
 
 
+struct SetMetaData: Identifiable {
+    var id = UUID().uuidString
+    var set: [Set] // Fix the array declaration
+    var setDate: Date
+}
+
+
+var sets: [SetMetaData] = []
+
+func saveToSets(warmUp: Set.WarmUp, preSet: Set.PreSet, mainSet: Set.MainSet, warmDown: Set.WarmDown, date: Date) {
+    // Create Set objects from the provided input
+    let warmUpSet = Set(sets: warmUp.set.sets, notes: warmUp.set.notes)
+    let preSetSet = Set(sets: preSet.set.sets, notes: preSet.set.notes)
+    let mainSetSet = Set(sets: mainSet.set.sets, notes: mainSet.set.notes)
+    let warmDownSet = Set(sets: warmDown.set.sets, notes: warmDown.set.notes)
+    
+    print("Warm Up Sets: \(warmUpSet.sets), Warm Up Notes: \(warmUpSet.notes)")
+    print("Pre Sets: \(preSetSet.sets), Pre Notes: \(preSetSet.notes)")
+    print("Main Sets: \(mainSetSet.sets), Main Notes: \(mainSetSet.notes)")
+    print("Warm Down Sets: \(warmDownSet.sets), Warm Down Notes: \(warmDownSet.notes)")
+    print("Date: \(date)")
+    
+    // Create SetMetaData object
+    let setMetaData = SetMetaData(set: [warmUpSet, preSetSet, mainSetSet, warmDownSet], setDate: date)
+    
+    // Append SetMetaData to sets array
+    sets.append(setMetaData)
+}
--- Workout-Log/Workout-Log/View/SetMaker.swift
+++ Workout-Log/Workout-Log/View/SetMaker.swift
@@ -8,10 +8,72 @@
 import SwiftUI
 
 struct SetMaker: View {
+    @State private var warmUpSets: String = ""
+    @State private var warmUpNotes: String = ""
+    
+    @State private var preSets: String = ""
+    @State private var preNotes: String = ""
+    
+    @State private var mainSets: String = ""
+    @State private var mainNotes: String = ""
+    
+    @State private var warmDownSets: String = ""
+    @State private var warmDownNotes: String = ""
+    
+    @State private var selectedDate = Date() // Selected date
+    
     var body: some View {
-        Text("This is the Set Maker App")
+        Form {
+            Section(header: Text("Date")) {
+                DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
+                    .datePickerStyle(.compact)
+            }
+            
+            createSection(withHeader: "Warm Up", textEditor: $warmUpSets, textField: $warmUpNotes)
+            createSection(withHeader: "Pre Set", textEditor: $preSets, textField: $preNotes)
+            createSection(withHeader: "Main Set", textEditor: $mainSets, textField: $mainNotes)
+            createSection(withHeader: "Warm Down", textEditor: $warmDownSets, textField: $warmDownNotes)
+            
+            Button("Save Work Out") {
+                let newWarmUp = Set.WarmUp(set: Set(sets: warmUpSets, notes: warmUpNotes))
+                // Save newWarmUp to Core Data
+                
+                let newPreSet = Set.PreSet(set: Set(sets: preSets, notes: preNotes))
+                // Save newPreSet to Core Data
+                
+                let newMainSet = Set.MainSet(set: Set(sets: mainSets, notes: mainNotes))
+                // Save newMainSet to Core Data
+                
+                let newWarmDown = Set.WarmDown(set: Set(sets: warmDownSets, notes: warmDownNotes))
+                // Save newWarmDown to Core Data
+                
+                let newDate = selectedDate
+                
+                saveToSets(warmUp: newWarmUp, preSet: newPreSet, mainSet: newMainSet, warmDown: newWarmDown, date: newDate)
+                
+                
+                
+                
+            }
+        }
+        
     }
 }
+
+// Function to create a consistent layout for TextEditor and TextField
+func createSection(withHeader header: String, textEditor: Binding<String>, textField: Binding<String>) -> some View {
+    Section(header: Text(header)) {
+        VStack {
+            TextEditor(text: textEditor)
+                .frame(minHeight: 100)
+                .border(Color.gray, width: 1)
+                .padding(.top, 10)
+            TextField("Focus", text: textField)
+        }
+    }
+}
+
+
 
 struct SetMaker_Previews: PreviewProvider {
     static var previews: some View {
